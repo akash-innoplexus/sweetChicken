fdbd6a29b84b532a0ac8876a9c17bd1af1b2c9d0
Akash <akash.mishra@innoplexus.com>
2016-06-30 14:43:05 +0530

    test.js changes

--- app/tests/category.server.model.test.js
+++ app/tests/category.server.model.test.js
@@ -22,14 +22,40 @@ describe('Category Model', function() {
 
 	describe('Saving', function() {
 
-		it('saves new record');
+		it('saves new record', function(done) {
+			var category = new Category({
+				name: 'Beverages',
+				description: 'Soft drinks, coffees, teas, beers and ales'
+			});
 
-		it('throws validation error when name is empty');
+			category.save(function(err, saved) {
+				should.not.exist(err);
+				done();
+			});
+		});
 
-		it('throws validation error when name is longer than 15 characters');
+		it('throws validation error when name is empty', function(done) {
+			var category = new Category({
+				description: 'Soft drinks, coffees, teas, beers and ales'
+			});
+
+			category.save(function(err) {
+				should.exist(err);
+				err.errors.name.message.should.equal('name cannot be blank');
+				done();
+			});
+		});
+
+		it('throws validation error when name is longer than 15 characters', function(done) {
+			var category = new Category({
+				name: 'Grains/Cereals/Chocolates'
+			});
+
+			category.save(function)
+		});
 
 		it('throws validation error for duplicate category name');
-		
+
 	});
 
 	// beforeEach(function(done) {
